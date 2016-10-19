739c06e52c2547d3fa995a3917d407c5fcba06fd
Nick Chirico <nickchirico07@gmail.com>
2016-10-19 17:37:09 -0400

    better command line argument support. note better != good.

--- uptime3.py
+++ uptime3.py
@@ -10,69 +10,75 @@ import sys
 def sincewhen(time):
 	delta = datetime.timedelta(seconds=time)
 	origin = str ( datetime.datetime.now() - delta )
-	fo = origin.replace(" ","-")
-	fo = fo.replace(":","-")
-	fo = fo.replace(":","-")
-	fo = fo.split(".",1)[0]
-	fo = fo.split("-")
+	formatted = origin.replace(" ","-")
+	formatted = formatted.replace(":","-")
+	formatted = formatted.replace(":","-")
+	formatted = formatted.split(".",1)[0]
+	formatted = formatted.split("-")
 
 	# Get month from provided number
-	since = calendar.month_name[int(fo[1])]
+	since = calendar.month_name[int(formatted[1])]
 	# Everyhting date related
-	since += " " + fo[2].replace("0","") + " " + fo[0] + " at "
+	since += " " + formatted[2].replace("0","") + " " + formatted[0] + " at "
 	# everything time related
-	since += fo[3] + ":" + fo[4] + "."
+	since += formatted[3] + ":" + formatted[4] + "."
 
 	print("Up since:", since)
 
-
-
-def main():
-	## read up to the first period of /proc/uptime
-	f = open('/proc/uptime', 'r')
-	time = int ( f.read().split('.',1)[0] )
-
-	# Actual calculations - > I decided to do it from the bottom up this time around
+def up(time):
+	# Actual Calculations
 	## Minutes
 	minutes = int (( time/60 ) % 60 )
 	overflow = int (( time/60 ) - ( minutes ))
-
 	## Hours
 	hours = int (( overflow/60 ) % 24 )
 	overflow = int (( overflow/60 ) - ( hours ))
-
 	## Days
 	days = int (( overflow/24 ) % 24 )
 	overflow = int (( overflow/24 ) - ( days ))
-
 	## Weeks
 	weeks = int (( overflow/7 ) % 7 )
 
 
 	# This will be printed later. Concat on relevant info
 	output = "Uptime: "
-
 	if weeks >= 1:
 		output = output + str ( int ( weeks ) ) + " weeks, "
-
 	if days >= 1:
 		output = output + str ( int ( days ) ) + " days, "
-
 	if hours >= 1:
 		output = output + str ( int ( hours ) ) + " hours, "
-
 	if minutes >= 1:
 		output = output + str ( int ( minutes ) ) + " minutes."
 	# Let's be proper now. In case this is done on the hour mark.
 	else:
 		output += "."
-
 	print(output)
 
+
+def main():
+	# read up to the first period of /proc/uptime
+	f = open('/proc/uptime', 'r')
+	time = int ( f.read().split('.',1)[0] )
+
+	# So I have no idea how to properly implement flags.
+	# The plan is 0 = default, 1 = orgin, 2 = both.
+	to_print = 0
 	if (len(sys.argv) > 1 ):
 		for arg in sys.argv:
 			if arg == "-o" or arg == "--origin":
-				sincewhen(time)
+				if( to_print != 2):
+					to_print=1
+			if  arg == "-a" or arg == "--all":
+				to_print=2
+
+	if ( to_print == 0 ):
+		up(time)
+	if ( to_print == 1 ):
+		sincewhen(time)
+	if ( to_print == 2 ):
+		up(time)
+		sincewhen(time)
 
 
 
